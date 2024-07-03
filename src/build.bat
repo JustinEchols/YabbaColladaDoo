0df9915cdc0bfed9f79c88e90558613e2809d274
Justin Echols <justindawsonechols@gmail.com>
2024-07-03 15:36:17 -0500

    Enable msvc command line tools in build.bat

--- src/build.bat
+++ src/build.bat
@@ -1,19 +1,31 @@
 @echo off
 
+REM Find latest Visual Studio installation via vswhere.exe to use MSVC toolset from command line.
+setlocal enabledelayedexpansion
+where /Q cl.exe || (
+  set __VSCMD_ARG_NO_LOGO=1
+  for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set "VS=%%i"
+  if "!VS!" equ "" (
+    echo ERROR: Visual Studio installation not found
+    exit /b 1
+  )  
+  call "!VS!\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
+)
+if "%VSCMD_ARG_TGT_ARCH%" neq "x64" (
+  echo ERROR: please run this from MSVC x64 native tools command prompt, 32-bit target is not supported!
+  exit /b 1
+)
+
 set MainFile=main.cpp
-REM set CommonCompilerFlags= -Od -nologo -W4 -wd4100 -wd4201 -wd4996 -wd4505 -Zi -DGLEW_STATIC=1 -D__CRT_SECURE_NO_WARNINGS=1 -DRENDER_TEST=1
-REM set CommonLinkerFlags=-incremental:no gdi32.lib user32.lib shell32.lib msvcrt.lib opengl32.lib winmm.lib glew32s.lib glfw3.lib
-REM set IncludeDirectories= /I "../dependencies/GLFW64/include" /I "../dependencies/GLEW/include"
-REM set LibDirectories= /LIBPATH:"../dependencies/GLFW64/lib-vc2019" /LIBPATH:"../dependencies/GLEW/lib/Release/x64"
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
 
-REM Render test compile
-REM cl ..\src\%MainFile% %CommonCompilerFlags% %IncludeDirectories% /link /NODEFAULTLIB:"LIBCMT" %LibDirectories% %CommonLinkerFlags%
+REM Conversion non-optimized
+REM cl ..\src\%MainFile% -Od -nologo -W4 -wd4100 -wd4201 -wd4505 -Zi -D_CRT_SECURE_NO_WARNINGS=1
 
-REM Conversion compile
-cl ..\src\%MainFile% -Od -nologo -W4 -wd4100 -wd4201 -wd4505 -Zi -D_CRT_SECURE_NO_WARNINGS=1
+REM Conversion optimized
+cl ..\src\%MainFile% -O2 -nologo -W4 -wd4100 -wd4201 -wd4505 -Zi -D_CRT_SECURE_NO_WARNINGS=1
 popd
 
 
